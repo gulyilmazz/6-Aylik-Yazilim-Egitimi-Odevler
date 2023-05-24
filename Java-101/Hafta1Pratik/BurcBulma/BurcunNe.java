9b4ee71e07bf4b11d29a455229731eec13c11af7
Gul-yilmaz <77543750+Gul-yilmaz@users.noreply.github.com>
2023-05-24 15:29:13 +0300

    BurcunNe

--- Java-101/Hafta1Pratik/BurcBulma/BurcunNe.java
+++ Java-101/Hafta1Pratik/BurcBulma/BurcunNe.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class BurcBulma {
+public class BurcunNe {
     public static void main(String[] args) {
        int month,day;
        Scanner input =new Scanner(System.in);
