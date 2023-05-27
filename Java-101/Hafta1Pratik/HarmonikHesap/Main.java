caec333603974439e6d8d0416f1a6e45b4f0fbd1
Gul-yilmaz <yilmazlutfiyegul@gmail.com>
2023-05-27 19:42:49 +0300

    Harmonik

---
+++ Java-101/Hafta1Pratik/HarmonikHesap/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("N sayısını giriniz: ");
+        int n = inp.nextInt();
+        double result = 0.0;
+
+        for(double i =1; i<= n; i++){
+            result += (1/i);
+        }
+        System.out.println(result);
+    }
+}
